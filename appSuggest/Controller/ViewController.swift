15bf7958072be7b010381a2f31cad82c13e57629
Rodrigo Longhi Guimarães <rguimaraes@alunos.utfpr.edu.br>
2017-10-26 16:31:53 -0200

    Tableview infinita

--- appSuggest/Controller/ViewController.swift
+++ appSuggest/Controller/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var elementos = [1,2,3,4,5,6]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return elementos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -27,14 +29,27 @@ class ViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tableView.dataSource = self
-//        tableView.delegate = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func carregarMaisDados(){
+        let novaLista = [1,2,3,4,5,6]
+        elementos.append(contentsOf: novaLista)
+        
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let ultimoItem = elementos.count - 1
+        if indexPath.row == ultimoItem {
+            carregarMaisDados()
+        }
+    }
 
 }
 
